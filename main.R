645b93debc1d35d22adec5a27febc80dbf77c1a1
maryseben <marysebenga@gmail.com>
2025-06-10 11:37:16 +0200

    first maryse

--- main.R
+++ main.R
@@ -13,3 +13,19 @@ doublon <- function(data) {
 }
 
 doublon(data = data)
+
+
+supprime_lignes_incompletes <- function(vessel.total.clean) {
+  vessel.total.clean <- as.data.frame(lapply(vessel.total.clean, function(col) {
+    if (is.character(col)) {
+      # Remplace les cellules contenant \N ou \n par NA
+      col[grepl("\\\\N", col) | grepl("\n", col)] <- NA  
+    }
+    return(col)
+  }))
+  print(rowSums(is.na(vessel.total.clean)))  # Affiche le nombre de NA par ligne
+  data_sans_na <- vessel.total.clean[complete.cases(vessel.total.clean), ]  # Supprime les lignes avec NA
+  return(data_sans_na)
+}
+result <- supprime_lignes_incompletes(vessel.total.clean)
+vessel.total.clean
